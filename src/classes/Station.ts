ecf00065d494185669f3bcd29e679c089264136b
gbrochard.dev <gbrochard.dev@gmail.com>
2024-06-05 16:54:26 +0200

    first commit

---
+++ src/classes/Station.ts
@@ -0,0 +1,34 @@
+export class Station {
+
+    private _nom: string
+    private _longitude: number
+    private _latitude: number
+
+    constructor(nom: string, longitude: number, latitude: number) {
+        this._nom = nom
+        this._latitude = latitude
+        this._longitude = longitude
+    }
+
+
+    getNom(): string {
+        return this._nom
+    }
+    setNom(nom: string): void {
+        this._nom
+    }
+
+    getLongitude(): number {
+        return this._longitude
+    }
+    setLongitude(longitude: number): void {
+        this._longitude = longitude
+    }
+
+    getLatitude(): number {
+        return this._latitude
+    }
+    setLatitude(latitude: number) {
+        this._latitude = latitude
+    }
+}
